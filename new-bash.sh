b54947dbcf63f2637a40e9db9b195c2e667c5566
Germano Gualberto <gmanopoliano@gmail.com>
2018-12-09 04:23:58 -0300

    geração de logs de execução concluida

--- new-bash.sh
+++ new-bash.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+LOG='commands.log'
+TIME='/usr/bin/time -po tmp '
+
+read comando
+#echo $comando
+$TIME $comando
+
+#cat tmp
+time_sys=$(grep "sys" tmp | cut -d' ' -f2 )
+time_user=$(grep "user" tmp | cut -d' ' -f2 )
+#user [ command ] mes/ano time_sys time_user
+echo `whoami` [ $comando ] $(date '+%d/%b/%Y') $time_sys $time_user >> $LOG
