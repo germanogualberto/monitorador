1a4833b4cbe65aca19ade0986e4e66052d5854ba
Germano Gualberto <gmanopoliano@gmail.com>
2018-12-16 00:15:29 -0300

    bug guardar comandos corrigido

--- new-bash.sh
+++ new-bash.sh
@@ -2,12 +2,27 @@
 LOG='commands.log'
 TIME='/usr/bin/time -po tmp '
 
+while true; do
+echo `whoami`@`hostname`:`pwd`$ 
 read comando
+if [[ "$comando" == *"cd"* ]];then
+echo `whoami` [ $comando ] $(date '+%d/%b/%Y') 0.00 0.00 >> $LOG
+pwd
+$comando
+pwd
+#cd ..
+#pwd
+else
+
 #echo $comando
-$TIME $comando
+echo $comando | $TIME /bin/bash
 
 #cat tmp
 time_sys=$(grep "sys" tmp | cut -d' ' -f2 )
 time_user=$(grep "user" tmp | cut -d' ' -f2 )
 #user [ command ] mes/ano time_sys time_user
 echo `whoami` [ $comando ] $(date '+%d/%b/%Y') $time_sys $time_user >> $LOG
+
+#echo "`whoami`@`hostname`:`pwd`$ "
+fi
+done
