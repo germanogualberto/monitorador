eea658f480897dacda810584db130bd268be67fa
Germano Gualberto <gmanopoliano@gmail.com>
2018-12-16 01:28:52 -0300

    relatorios prontos

--- rel.sh
+++ rel.sh
@@ -3,15 +3,17 @@
 #usage
 # ./rel.sh nome_usuario
 
-NOME_USER=`whoami` #$1
+NOME_USER=$1
 #1) quanto tempo de CPU (divididos em sys e user) o usuário consumiu durante um mês; 
 
 echo "Tempo de CPU gasto por $NOME_USER"
 echo "sys user"
-cat commands.log | grep "$NOME_USER" | awk '{ n=n+$7; m=m+$8} END{print n,m}'
-
-echo "Comando mais usados:"
-cat commands.log | grep "$NOME_USER" | 
+cat commands.log | grep "$NOME_USER" | grep `date '+%b'`| awk '{ n=n+$7; m=m+$8} END{print n,m}'
 
 
 #2) um histograma da utilização de comandos pelo usuário (ou seja, quantas vezes cada comando foi usado em um mês).
+echo "--------------------"
+echo "Comandos mais usados no ultimo mês:"
+echo "Quantidade | Comando"
+awk '{print $3}' commands.log | sort | uniq -c
+
